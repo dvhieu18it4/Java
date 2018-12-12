70c1eb414ba2315edb7577c3dc3f6d0e61ac90a3
dvhieu18it4 <44391096+dvhieu18it4@users.noreply.github.com>
2018-12-12 11:08:12 +0700

    Update cuoikhoa-tabletaoSV.java

--- cuoikhoa-tabletaoSV.java
+++ cuoikhoa-tabletaoSV.java
@@ -1,4 +1,4 @@
-package gui;
+package SinhVien;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
@@ -6,18 +6,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
-
+import java.awt.Color;
 import javax.swing.JButton;
 
-public class TaoSV extends JFrame {
+public class FormSV extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField tfID;
@@ -30,7 +32,7 @@ public class TaoSV extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					TaoSV frame = new TaoSV();
+					FormSV frame = new FormSV();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -42,7 +44,7 @@ public class TaoSV extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public TaoSV() {
+	public FormSV() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -51,48 +53,30 @@ public class TaoSV extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblID = new JLabel("ID");
-		lblID.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblID.setBounds(28, 51, 46, 21);
+		lblID.setForeground(Color.BLACK);
+		lblID.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblID.setBounds(34, 49, 44, 20);
 		contentPane.add(lblID);
 		
 		tfID = new JTextField();
-		tfID.setBounds(84, 51, 145, 21);
+		tfID.setBounds(105, 49, 118, 20);
 		contentPane.add(tfID);
 		tfID.setColumns(10);
 		
 		JLabel lblName = new JLabel("Name");
-		lblName.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblName.setBounds(28, 93, 46, 14);
+		lblName.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblName.setBounds(32, 93, 44, 20);
 		contentPane.add(lblName);
 		
 		tfName = new JTextField();
-		tfName.setBounds(84, 86, 145, 21);
+		tfName.setBounds(105, 94, 118, 19);
 		contentPane.add(tfName);
 		tfName.setColumns(10);
 		
+		// BUTTON CREATE
 		JButton btnCreate = new JButton("Create");
-		btnCreate.setBounds(84, 135, 89, 23);
+		btnCreate.setBounds(71, 138, 89, 23);
 		contentPane.add(btnCreate);
-		// tao Update
-		JButton btnUpdate = new JButton("Update");
-		btnUpdate.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				try {
-					connectDB();
-					stmt =conn.createStatement();
-					stmt.executeUpdate("Update sinhvien set Name ='"+tfName.getText()+"' " + " where ID ='"+tfID.getText()+"' ");
-				} catch (Exception e2) {
-					// TODO: handle exception
-				}
-			}
-		});
-		btnUpdate.setBounds(183, 135, 89, 23);
-		contentPane.add(btnUpdate);
-		
-		JButton btnDelete = new JButton("Delete");
-		btnDelete.setBounds(282, 135, 89, 23);
-		contentPane.add(btnDelete);
-		
 		btnCreate.addActionListener(new ActionListener() {
 			
 			@Override
@@ -100,30 +84,66 @@ public class TaoSV extends JFrame {
 				// TODO Auto-generated method stub
 				try {
 					connectDB();
-					stmt =conn.createStatement();
-					stmt.executeUpdate("Insert into sinhvien(ID,Name) "+"value('"+tfID.getText()+"','"+tfName.getText()+"')");
+					stmt=conn.createStatement();
+					stmt.executeUpdate("insert into sinhvien(ID,Name) values ('"+tfID.getText()+"','"+tfName.getText()+"')");
 				} catch (Exception e2) {
 					// TODO: handle exception
+				}
+			}
+		});
+		
+		// BUTTON UPDATE
+		JButton btnUpdate = new JButton("Update");
+		btnUpdate.setBounds(194, 138, 89, 23);
+		contentPane.add(btnUpdate);
+		btnUpdate.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				try {
+					connectDB();
+					stmt = conn.createStatement();
+					stmt.executeUpdate("Update SinhVien set Name ='"+tfName.getText()+"' " + "where ID='"+tfID.getText()+"' ");
+				} catch (Exception e2) {
+					// TODO: handle exception
+				}
+			}
+		});
+		
+		// Tim Kiem
+		JButton btnTim = new JButton("Tim Kiem");
+		btnTim.setBounds(307, 138, 89, 23);
+		contentPane.add(btnTim);
+		btnTim.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				try {
+					connectDB();
+					stmt =conn.createStatement();
+					String sql ="SELECT Name From sinhvien Where ID= ?";
+					conn.prepareStatement(sql);
+				} catch (Exception e2) {
+					// TODO: handle exception
+					
 				}
 			}
 		});
 	}
 	
 	//tao doi tuong connect
-	Connection conn;
-	Statement stmt;
-	//tao ham connect DB
-	public void connectDB() {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost/qlsv", "root", "");
-			System.out.println("Connect Sucess");
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
+		Connection conn;
+		Statement stmt;
+		//tao ham connect DB
+		public void connectDB() {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/ql", "root", "");
+				System.out.println("Connect Sucess");
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
 		}
-	}
-	
 }
-
-
